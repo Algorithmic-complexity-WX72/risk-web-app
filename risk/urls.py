e7639fff36c068068ecf1b0fdd2a3c6ba8ea6671
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-06-18 10:22:20 -0500

    feat(html): connection between python and html

--- risk/urls.py
+++ risk/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from risk import views
 
 urlpatterns =[
-    path("risk/", views.suma, name="risk") #join views
+    path("risk/home", views.home, name="risk"), #join views
+    path("risk/app",views.app, name ="risk app")
 
 ]
--- risk/views.py
+++ risk/views.py
@@ -4,10 +4,19 @@ from django.template import loader
 
 
 # Create your views here.
-def suma(request):
+def home(request):
     sumtotal = 1 + 2
-    template = loader.get_template('webapp.html') #loading template html
-    context = {'risk' : sumtotal} #passing context to template          #risk is how you call it in html
-    return HttpResponse(template.render(context, request)) #returning template with context
+    templateHome = loader.get_template('home.html') #loading template html
+    context = {'riskHome' : sumtotal} #passing context to template          #risk is how you call it in html
+    return HttpResponse(templateHome.render(context, request)) #returning template with context
 
     # return HttpResponse(f"result is {sumtotal}")
+def app(request):
+    app_value = suma()
+    templateApp = loader.get_template('webapp.html')
+    context = {'suma' : app_value }
+    return HttpResponse(templateApp.render(context, request))
+
+def suma():
+    trying = 1+2
+    return trying
