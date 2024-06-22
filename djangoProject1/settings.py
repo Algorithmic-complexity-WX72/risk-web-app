d8d8a21dc426484844030be5e27a287232ff5e2b
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-06-22 16:31:53 -0500

    feat(settings): white noise added

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import whitenoise
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'risk.apps.RiskConfig',
+    'whitenoise.runserver_nostatic',
     'djangoProject1'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -118,20 +121,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Add this line
 
 #This let me see the svg but the function cant create other again
 #
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    ]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     ]
 
 #This doesnt let me see the svg but the function can create other again
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static'
 
 
+# White Noise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
